73d796b37734aec5f79a24a0fa39462f53c92938
Aurélien PIERRE <contact@aurelienpierre.com>
2022-10-04 21:19:12 +0200

    Instanciate protocols in a dict directly

--- src/secretary.py
+++ src/secretary.py
@@ -7,16 +7,16 @@ import os
 
 class Secretary(object):
   def load_connectors(self):
-    for key in self.__dict__:
+    for key in self.protocols:
       if key in self.config_file and key in self.protocols:
-        self.__dict__[key].init_connection(self.config_file[key]["server"],
-                                           self.config_file[key]["user"],
-                                           self.config_file[key]["password"],
-                                           int(self.config_file[key]["entries"]))
+        self.protocols[key].init_connection(self.config_file[key]["server"],
+                                            self.config_file[key]["user"],
+                                            self.config_file[key]["password"],
+                                            int(self.config_file[key]["entries"]))
   def close_connectors(self):
-    for key in self.__dict__:
-      if key in self.config_file and key in self.protocols:
-        self.__dict__[key].close_connection()
+    for key in self.protocols:
+      if key in self.config_file:
+        self.protocols[key].close_connection()
 
   def filters(self, filters:dict):
     # Launch the method that processes the filter loop for this server
@@ -26,7 +26,7 @@ class Secretary(object):
       protocol = filters[key]["protocol"]
 
       # Check that we have a server and an active connection for this protocol
-      server_instance = self.__dict__[protocol]
+      server_instance = self.protocols[protocol]
       if not (server_instance and server_instance.connection_inited):
         print("We have no active connector for the protocol %s, check that you defined your credentials in `settings.ini` for it." % protocol)
         return
@@ -46,18 +46,10 @@ class Secretary(object):
     self.logfile = open(os.path.join(subfolder_path, "sync.log"), 'a')
 
     ## Open all servers for which we have credentials in config file
-
-    # Declare the list of all supported protocols. Should match the sections of config files
-    # as well as the protocol prefix in filter filenames.
-    self.protocols = ["imap"]
-
-    # Instanciate here all implemented server classes
-    self.imap = mailserver.MailServer(self.logfile)
-    self.smtp = None
-    self.carddav = None
-    self.calddav = None
-    self.googlecal = None
-    self.mysql = None
+    # Declare the list of all supported protocols and instanciate their server classes here.
+    # Keys should match the sections of config files and the protocol prefix in filter filenames.
+    self.protocols = {}
+    self.protocols["imap"] = mailserver.MailServer(self.logfile)
 
     # Load all implemented connectors
     self.load_connectors()
