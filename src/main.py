bfb659a80c9dbee993a8fb2b7dc6e19ba55e871e
Aurélien PIERRE <contact@aurelienpierre.com>
2022-12-18 22:51:17 +0100

    Main: add a single param for debug on one config folder only

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import os
 import utils
 import re
 import time
+import sys
 
 from secretary import Secretary
 
@@ -16,9 +17,12 @@ parser.add_argument('path', metavar='path', type=str,
                     help='path of the config directory')
 parser.add_argument('mode', metavar='mode', type=str,
                     help='`process` to run only the processing filters (prefixed with 2 digits), \n`learn` to run the learning filters (prefixed with LEARN), \n`dryrun` to only test server connections')
+parser.add_argument('-s', '--single', type=str, help="path of the only config subfolder that will be processed, typically to run filters on a single email account for debugging.")
+
 args = parser.parse_args()
 path = os.path.abspath(args.path)
 mode = args.mode
+single = args.single
 
 # Get the common filters if any
 filters = utils.find_filters(os.path.join(path, "common"), { }, mode)
@@ -27,6 +31,14 @@ filters = utils.find_filters(os.path.join(path, "common"), { }, mode)
 # process the filters
 subfolders = sorted(os.listdir(path))
 subfolders.remove("common")
+
+# In single mode, process only the specified subfolder
+if single:
+  if single in subfolders:
+    subfolders = [single]
+  else:
+    print("The subfolder %s has not been found in %s. Abort" % (single, path))
+    sys.exit(1)
 
 for dir in subfolders:
   # Manage concurrence by disabling editing over a captured folder
@@ -52,3 +64,4 @@ for dir in subfolders:
 
 
 print("\nGlobal execution took %.2f s. Mind this if you use cron jobs." % (time.time() - ts))
+sys.exit(0)
