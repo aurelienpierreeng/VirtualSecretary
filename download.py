21721473782a653974f6880a97cdfce1585ce932
Aurélien PIERRE <contact@aurelienpierre.com>
2025-05-03 15:52:03 +0200

    Update dependencies

--- download.py
+++ download.py
@@ -1,7 +1,10 @@
 """Download NLTK corpora"""
+import os
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
 
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
